894780b66d7025bb36a2bd31b3035869fec147f4
Amoralih <alicebadwonderland@gmail.com>
2022-05-20 16:57:41 -0300

    Aprendendo a usar ponteiros

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -1,17 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"modulo/auxiliar"
+//"fmt"
+//"modulo/auxiliar"
+//"github.com/badoux/checkmail"
+//"github.com/badoux/checkmail"
 
-	"github.com/badoux/checkmail"
-	//"github.com/badoux/checkmail"
-)
+//func main() {
+//fmt.Println("Escrevendo do arquivo main")
+//auxiliar.Escrever()
+//Erro := apenas para checar caso dê erro na validação.
+//erro := checkmail.ValidateFormat("alicebadwonderland@gmail.com")
+//fmt.Println(erro)
 
-func main() {
-	fmt.Println("Escrevendo do arquivo main")
-	auxiliar.Escrever()
-	//Erro := apenas para checar caso dê erro na validação.
-	erro := checkmail.ValidateFormat("alicebadwonderland@gmail.com")
-	fmt.Println(erro)
-}
+//}
