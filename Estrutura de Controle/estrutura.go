639cb7a8c39eaadc70855d2d00e56477cb139f5c
amoralih <85180107+amoralih@users.noreply.github.com>
2022-06-02 19:56:13 -0300

    Aprendendo Estrutura de Controle em Go

---
+++ Estrutura de Controle/estrutura.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Estrutura de Controle")
+
+	number := 20
+
+	if  number > 100 {
+		fmt.Println("Maior que 110")
+	} else {
+		fmt.Println("Menor ou igual a 110")
+	}
+		
+}
